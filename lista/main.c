83dfc0375c0812017f46c65d9515faf4c17ad00e
Fabricio assuncao <fabricioassuncao855@outlook.com>
2023-03-29 23:43:03 -0300

    acho que agora tá certo

--- lista/main.c
+++ lista/main.c
@@ -10,10 +10,31 @@ struct Node
 void insert(struct Node *l, int valor)
 {
 	struct Node *node = (struct Node *)malloc(sizeof(struct Node));
-	l->val = valor;
-	l->next = node;
+	if (l->val == 0)
+	{
+		l->val = valor;
+		l->next = node;
+	}
+	else
+	{
+		while (l->next != NULL)
+		{
+			l = l->next;
+		}
+		l->next = node;
+		l->val = valor;
+	}
 
 	printf("Valor inserido: %d \n", l->val);
+}
+
+void printList(struct Node *l)
+{
+	while (l->next != NULL)
+	{
+		printf("Valor: %d \n", l->val);
+		l = l->next;
+	}
 }
 
 int main(void)
@@ -23,9 +44,14 @@ int main(void)
 	l->val = 0;
 	l->next = NULL;
 
-	insert(l, 10);
-	insert(l, 20);
-	insert(l, 30);
-	insert(l, 40);
+	insert(l, 1);
+	insert(l, 2);
+	insert(l, 3);
+	insert(l, 4);
+	insert(l, 5);
+	insert(l, 6);
+	insert(l, 7);
+
+	printList(l);
 	return 0;
 }
